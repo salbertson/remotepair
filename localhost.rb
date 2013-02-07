403a4f162153b02225b27fa4816ac4f442a6b8fe
Scott Albertson <scott@thoughtbot.com>
2013-02-07 15:46:20 -0800

    Move gateway patch and remove Tunnel

--- localhost.rb
+++ localhost.rb
@@ -1,4 +1,24 @@
-require './tunnel'
+require './net_ssh_gateway_patch'
 
-tunnel = Tunnel.new('50.116.19.132', 2222)
-tunnel.start
+# ssh -nvNT -R 2222:localhost:22 desi@50.116.19.132
+
+begin
+  gateway = Net::SSH::Gateway.new('50.116.19.132', 'desi', password: 'imawesome')
+  gateway.open_remote(22, 'localhost', 2222) do |remote_port, remote_host|
+    puts 'Tunnel is open ...'
+    Process.spawn(<<-TMUX)
+osascript -e 'tell app "Terminal"
+do script "cd #{Dir.pwd} && ruby localjoin.rb"
+end tell'
+    TMUX
+    begin
+      sleep 1 while true
+    rescue Interrupt
+      gateway.close_remote(remote_port, remote_host)
+      exit
+    end
+  end
+rescue Net::SSH::AuthenticationFailed
+  puts 'Failed to authenticate'
+  exit
+end
--- remotehost.rb
+++ remotehost.rb
@@ -2,18 +2,23 @@ require 'net/ssh/gateway'
 
 # ssh -nvNT -L 8000:localhost:2222 salbertson@50.116.19.132
 
-gateway = Net::SSH::Gateway.new('50.116.19.132', 'desi', password: 'imawesome')
-gateway.open('localhost', 2222, 8000) do |port|
-  puts 'Tunnel is open ...'
-  Process.spawn(<<-TMUX)
-osascript -e 'tell app "Terminal"
-do script "cd #{Dir.pwd} && ruby remotejoin.rb"
-end tell'
-  TMUX
-  begin
-    sleep 1 while true
-  rescue Interrupt
-    gateway.close(port)
-    exit
+begin
+  gateway = Net::SSH::Gateway.new('50.116.19.132', 'desi', password: 'imawesome')
+  gateway.open('localhost', 2222, 8000) do |port|
+    puts 'Tunnel is open ...'
+    Process.spawn(<<-TMUX)
+  osascript -e 'tell app "Terminal"
+  do script "cd #{Dir.pwd} && ruby remotejoin.rb"
+  end tell'
+    TMUX
+    begin
+      sleep 1 while true
+    rescue Interrupt
+      gateway.close(port)
+      exit
+    end
   end
+rescue Net::SSH::AuthenticationFailed
+  puts 'Failed to authenticate'
+  exit
 end
